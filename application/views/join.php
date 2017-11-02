6274ccfd5ab69aa80f7a0a2a05d674a3fe312d90
喂喂 <626218581@qq.com>
2017-11-02 22:15:56 +0800

    1102快完了-1

--- application/views/join.php
+++ application/views/join.php
@@ -26,6 +26,7 @@
     form .form-group{
 
     }
+    .help-block{font-size:12px;}
     .tip{
         border-top:1px solid #dfe2e5;
         border-bottom:1px solid #dfe2e5;
@@ -52,7 +53,7 @@
         <div class="col-sm-12 col-md-8">
             <h4 class="txt-shadow"><strong>创建你的个人账户</strong></h4>
             <div class="row">
-                <form method="post" action="/join" autocomplete="off" class="col-sm-12 col-md-8">
+                <form method="post" action="/join" autocomplete="off" class="col-sm-12 col-md-8" id="joinForm">
                     <?php if(isset($error_message))
                         echo '<div class="alert alert-danger alert-dismissible" role="alert" id="alert-danger">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
@@ -60,22 +61,35 @@
         </div>'
                     ?>
                     <div class="form-group">
-                        <label for="inputCode">注册码</label>
-                        <input type="text" class="form-control" id="inputCode" required>
+                        <label class="control-label" for="inputCode">注册码</label>
+                        <input type="text" class="form-control" name="inputCode" id="inputCode" required>
                         <p class="help-block">注册码是通过网站定期发放的。</p>
                     </div>
                     <div class="form-group">
-                        <label for="inputUser">用户名</label>
-                        <input type="text" class="form-control" id="inputUser" maxlength="20" required>
-                        <p class="help-block">你可以设置一个长度不超过 20 的用户名。</p>
+                        <label class="control-label" for="inputUser">用户名</label>
+                        <input type="text" class="form-control" name="inputUser" id="inputUser" maxlength="20" required>
+                        <p class="help-block">以字母开头，长度在 20 个字符(下划线、字母、数字)。</p>
                     </div>
                     <div class="form-group">
-                        <label for="inputPassword">密 码</label>
-                        <input type="password" class="form-control" id="inputPassword" required>
+                        <label class="control-label" for="inputPassword">密 码</label>
+                        <input type="password" class="form-control" name="inputPassword" id="inputPassword" required>
                         <p class="help-block">请你放心，我不会傻到直接明文保存。</p>
                     </div>
+                    <div class="form-group">
+                        <label class="control-label" for="inputSurname">姓 氏</label>
+                        <input type="text" class="form-control" name="inputSurname" id="inputSurname" required>
+                        <p class="help-block">你的姓氏将会在发布信息或留言时展示。</p>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label" for="inputSex">性 别</label>
+                        <select class="form-control" id="inputSex" name="inputSex">
+                            <option value="0">男</option>
+                            <option value="1">女</option>
+                        </select>
+                        <p class="help-block">你的性别会伴随姓氏展示在页面上。</p>
+                    </div>
                     <p class="help-block tip">当你按下「创建账户」按钮时，则表示你同意并坚决执行 iLiDan 守则( 是什么，我也还不知道 )。</p>
-                    <button type="submit" class="button button-primary button-rounded button-small">创建账户</button>
+                    <button type="submit" data-lock="true" data-unlock-txt="创建账户" data-lock-txt="创建中..." id="sendBtn" class="button button-primary button-rounded">创建账户</button>
                 </form>
             </div>
         </div>
@@ -93,6 +107,38 @@
 </div>
 
 <script type="text/javascript">
+    var submit_lock = true;
     function page_init(){
+        $("#joinForm").submit(function(){
+            ajax_submit();
+            return false;
+        });
+        $("#inputUser").change(function(){
+            var inputUser = $(this);
+            var objPar = inputUser.parent();
+            if(/^[a-zA-Z]{1}[A-Za-z0-9_]*$/.test(inputUser.val())){
+                if(objPar.hasClass('has-error')) objPar.removeClass('has-error');
+                submit_lock = false;
+            }else{
+                if(!objPar.hasClass('has-error')) objPar.addClass('has-error');
+                submit_lock = true;
+            }
+        });
+    }
+    function ajax_submit(){
+        if(submit_lock) return false;
+        var comment_uri = '/join';
+        ILIDAN_AJAX_OBJ = $("#sendBtn");
+        $.ajax({
+            url: comment_uri,
+            type:'post',
+            data:$('#joinForm').serialize(),
+            success:function(data){
+                if(data.code == 200) {
+                    alert('OK');
+                }
+                else alert(data.info);
+            }
+        });
     }
 </script>
